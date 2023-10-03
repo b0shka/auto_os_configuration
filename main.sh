6b4131e226d3de44bc72ba52445348c2cfd88539
b0shka <vanya.palam@ya.ru>
2023-10-03 22:39:18 +0500

    Updated vs code setup and added tools installation for golang

--- main.sh
+++ main.sh
@@ -62,19 +62,28 @@ install_backend_tools() {
 	go install github.com/sqlc-dev/sqlc/cmd/sqlc@latest
 
 	# migrate
-	VERSION_MIGRATE=$(jq -r '.apps.versions_backend_tools.migrate' $CONFIG_FILE)
-	curl -L https://github.com/golang-migrate/migrate/releases/download/$VERSION_MIGRATE/migrate.linux-amd64.tar.gz | tar xvz
-	sudo mv migrate /usr/bin/migrate
+	go install github.com/golang-migrate/migrate/v4/cmd/migrate@latest
+	# VERSION_MIGRATE=$(jq -r '.apps.versions_backend_tools.migrate' $CONFIG_FILE)
+	# curl -L https://github.com/golang-migrate/migrate/releases/download/$VERSION_MIGRATE/migrate.linux-amd64.tar.gz | tar xvz
+	# sudo mv migrate /usr/bin/migrate
 
 	# mockgen
-	sudo apt-get -y install mockgen
+	go install github.com/golang/mock/mockgen@latest
 
 	# swag
 	go install github.com/swaggo/swag/cmd/swag@latest
 
 	# golangci-lint
-	VERSION_GOLANGCI_LINT=$(jq -r '.apps.versions_backend_tools.golangci-lint' $CONFIG_FILE)
-	curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin $VERSION_GOLANGCI_LINT
+	go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
+	# VERSION_GOLANGCI_LINT=$(jq -r '.apps.versions_backend_tools.golangci-lint' $CONFIG_FILE)
+	# curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin $VERSION_GOLANGCI_LINT
+
+	# other
+	go install golang.org/x/tools/gopls@latest
+	go install golang.org/x/tools/cmd/goimports@latest
+	go install github.com/cweill/gotests/gotests@latest
+	go install github.com/fatih/gomodifytags@latest
+	go install github.com/go-delve/delve/cmd/dlv@latest
 }
 
 install_golang() {
@@ -337,8 +346,11 @@ configure_nautilus() {
 }
 
 configure_vscode() {
-	VSCODE_PATH=$(jq -r '.config.vscode' $CONFIG_FILE)
-	cp $VSCODE_PATH "$HOME/.config/Code/User/"
+	VSCODE_SETTINGS_PATH=$(jq -r '.config.vscode.settings' $CONFIG_FILE)
+	VSCODE_KEYBINDINGS_PATH=$(jq -r '.config.vscode.keybindings' $CONFIG_FILE)
+
+	cp $VSCODE_SETTINGS_PATH "$HOME/.config/Code/User/"
+	cp $VSCODE_KEYBINDINGS_PATH "$HOME/.config/Code/User/"
 
 	VSCODE_EXTENSIONS=$(jq -r '.vscode.extensions[]' $CONFIG_FILE)
 	for i in ${VSCODE_EXTENSIONS[@]}; do
