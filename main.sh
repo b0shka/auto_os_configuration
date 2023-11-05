175930d3957a3b0207090d93968be740a4769939
b0shka <vanya.palam@ya.ru>
2023-11-05 22:23:40 +0500

    Added decrypt backup archive

--- main.sh
+++ main.sh
@@ -464,10 +464,15 @@ create_tmux_sessions() {
 
 download_folders_from_mega() {
 	MEGA_BACKUP_NAME=$(jq -r '.mega.backup_name' $CONFIG_FILE)
+	MEGA_BACKUP_NAME_CRYPT=$(jq -r '.mega.backup_name_crypt' $CONFIG_FILE)
+	KEY_PASS=$(jq -r '.config.key_pass' $CONFIG_FILE)
 
-	mega-get /$MEGA_BACKUP_NAME $HOME
+	mega-get /$MEGA_BACKUP_NAME_CRYPT $HOME
+	openssl enc -aes-256-cbc -d -in $HOME/$MEGA_BACKUP_NAME_CRYPT -out $HOME/$MEGA_BACKUP_NAME -pass file:$HOME/$KEY_PASS
 	tar xf $HOME/$MEGA_BACKUP_NAME -C $HOME
+	
 	rm $HOME/$MEGA_BACKUP_NAME
+	rm $HOME/$MEGA_BACKUP_NAME_CRYPT
 }
 
 download_notes() {
